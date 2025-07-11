7c95c1df43e42fdcdeb16d19ac6e581e336387ad
Olivetta21 <ivancraftbr@gmail.com>
2025-07-11 18:44:18 -0400

    Improvement in the expandable panel

--- Site/gpstracker/src/components/PanelPages.vue
+++ Site/gpstracker/src/components/PanelPages.vue
@@ -1,20 +1,21 @@
 <template>
     <div id="home-page">
-        <div v-if="contpanel" id="content-panel"> 
-            <div id="content-header">
-                <span @click="contpanel = !contpanel" class="button-a toggle-panel"> &lt; </span>
-                <p>usuario</p>
+        <div id="content-panel">
+            <div @click="contpanel = !contpanel" id="toggle-panel" :class="['button-a', { closed: !contpanel }]">
+                <p> &lt; </p>
             </div>
-            <div>
-                <ul>
-                    <li>Item 1</li>
-                    <li>Item 2</li>
-                    <li>Item 3</li>
-                </ul>
+            <div v-if="contpanel" id="content"> 
+                <div id="content-header">
+                    <p>usuario</p>
+                </div>
+                <div>
+                    <ul>
+                        <li>Item 1</li>
+                        <li>Item 2</li>
+                        <li>Item 3</li>
+                    </ul>
+                </div>
             </div>
-        </div>
-        <div v-else id="content-panel" class="closed">
-            <p @click="contpanel = !contpanel" class="button-a toggle-panel"> &gt; </p>
         </div>
         <div id="pagina">
             <router-view/>
@@ -58,19 +59,28 @@ export default {
 }
 
 #content-panel {
+    background-color: #f0f0f0;
+    display: flex;
+    flex-direction: row-reverse;
+}
+
+#content {
     width: 300px;
     height: auto;
-    background-color: #f0f0f0;
-    padding: 20px;
-
-    display: flex;
-    flex-direction: column;
 }
-#content-panel.closed {
+
+#toggle-panel {
     padding: 5px;
     width: 25px;
-    align-items: center;
+    display: flex;
+    flex-direction: column;
     justify-content: center;
+    align-items: center;
+
+    border-left: 1px solid gray;
+}
+#toggle-panel.closed>p {
+    transform: rotate(180deg);
 }
 
 
@@ -85,15 +95,27 @@ export default {
         flex-direction: column-reverse;
     }
     #content-panel {
+        flex-direction: column;
+    }
+
+    #content {
         width: auto;
         height: 300px;
     }
-    #content-panel.closed {
+
+    #toggle-panel {
+        flex-direction: row;
         width: auto;
         height: 25px;
+        
+        border-left: 0;
+        border-bottom: 1px solid gray;
     }
-    .toggle-panel {
+    #toggle-panel>p {
         transform: rotate(-90deg);
+    }
+    #toggle-panel.closed>p {
+        transform: rotate(90deg);
     }
 }   
 </style>
