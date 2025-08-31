882c1003f3c8ca01d856ce2b53c2159c7647808c
Olivetta21 <ivancraftbr@gmail.com>
2025-08-31 16:40:39 -0400

    Optimized byte handling

--- Python/CryptLib/Crypt.py
+++ Python/CryptLib/Crypt.py
@@ -1,4 +1,3 @@
-from ctypes_ import UInt8 as byte
 from random import randint
 
 # Encryption config
@@ -15,36 +14,36 @@ if IV_SIZE + CHECKSUM_SIZE >= BUFFER_SIZE:
 class Crypt:
     
     def __init__(self):
-        self.__keys = [[byte(0) for _ in range(KEY_SIZE)] for _ in range(ROUNDS)]
+        self.__keys = [bytearray(KEY_SIZE) for _ in range(ROUNDS)]
 
     def generateSubkeys(self) -> None:
         for i in range(1, ROUNDS):
             for j in range(KEY_SIZE):
                 prevByte = self.__keys[i - 1][j]
-                self.__keys[i][j] = ((prevByte << 3) | (prevByte >> 5)) ^ (j + i * 7)
+                self.__keys[i][j] = (((prevByte << 3) & 0xFF) | (prevByte >> 5)) ^ (j + i * 7)
 
-    def encodeByte(self, byte_: byte, key: byte, mod: byte) -> byte:
+    def encodeByte(self, byte: int, key: int, mod: int) -> int:
         match mod:
             case 0:
-                byte_ ^= key
+                byte ^= key
             case 1:
-                byte_ += key
+                byte += key
             case _:
-                byte_ = ((byte_ << 4) | (byte_ >> 4)) ^ key        
-        return byte_
+                byte = ((byte << 4) | (byte >> 4)) ^ key
+        return byte & 0xFF
 
-    def decodeByte(self, byte_: byte, key: byte, mod: byte) -> byte:
+    def decodeByte(self, byte: int, key: int, mod: int) -> int:
         match mod:
             case 0:
-                byte_ ^= key
+                byte ^= key
             case 1:
-                byte_ -= key
+                byte -= key
             case _:
-                tmp = byte_ ^ key
-                byte_ = ((tmp >> 4) | (tmp << 4))
-        return byte_
+                tmp = byte ^ key
+                byte = ((tmp >> 4) | (tmp << 4))
+        return byte & 0xFF
     
-    def enc_dec_byteArray(self, byte_array: list[byte], round: int, encode: bool) -> None:
+    def enc_dec_byteArray(self, byte_array: bytearray, round: int, encode: bool) -> None:
         for i in range(len(byte_array)):
             mod = (i + round) % 3
             key = self.__keys[round][i % KEY_SIZE]
@@ -53,37 +52,37 @@ class Crypt:
             else:
                 byte_array[i] = self.decodeByte(byte_array[i], key, mod)
 
-    def genAndInsertIV(self, byte_array: list[byte]) -> bool:
+    def genAndInsertIV(self, byte_array: bytearray) -> bool:
         if (len(byte_array) + IV_SIZE + CHECKSUM_SIZE > BUFFER_SIZE):
             return False
         temp = byte_array.copy()
-        byte_array += [None] * IV_SIZE
+        byte_array += bytearray(IV_SIZE)
         byte_array[IV_SIZE:] = temp[:]
         for i in range(IV_SIZE):
-            byte_array[i] = byte(randint(0, 255))
+            byte_array[i] = randint(0, 255)
         return True
-    
-    def genAndInsertChecksum(self, byte_array: list[byte]) -> bool:
+
+    def genAndInsertChecksum(self, byte_array: bytearray) -> bool:
         if (len(byte_array) + CHECKSUM_SIZE > BUFFER_SIZE):
             return False
         checksum = 0
         for i in range(len(byte_array)):
             checksum += int(byte_array[i])
         for i in range(CHECKSUM_SIZE):
-            byte_array.append(byte((checksum >> (8 * (CHECKSUM_SIZE - 1 - i))) & 0xFF))
+            byte_array.append((checksum >> (8 * (CHECKSUM_SIZE - 1 - i))) & 0xFF)
         return True
 
-    def de_ob_fuscateWithIV(self, byte_array: list[byte]) -> bool:
+    def de_ob_fuscateWithIV(self, byte_array: bytearray) -> bool:
         if (len(byte_array) <= IV_SIZE):
             return False
         for i in range(IV_SIZE, len(byte_array)):
             byte_array[i] ^= byte_array[i % IV_SIZE]
         return True
 
-    def testAndRemoveChecksum(self, byte_array: list[byte]) -> bool:
+    def testAndRemoveChecksum(self, byte_array: bytearray) -> bool:
         constructedChecksum = 0
         for i in range(CHECKSUM_SIZE):
-            constructedChecksum |= (int(byte_array[len(byte_array) - CHECKSUM_SIZE + i]) << (8 * (CHECKSUM_SIZE - 1 - i)))
+            constructedChecksum |= byte_array[len(byte_array) - CHECKSUM_SIZE + i] << (8 * (CHECKSUM_SIZE - 1 - i))
         for i in range(len(byte_array) - CHECKSUM_SIZE):
             constructedChecksum -= int(byte_array[i])
         if constructedChecksum != 0:
@@ -91,7 +90,7 @@ class Crypt:
         byte_array[:] = byte_array[:len(byte_array) - CHECKSUM_SIZE]
         return True
 
-    def encrypt(self, byte_array: list[byte]) -> bool:
+    def encrypt(self, byte_array: bytearray) -> bool:
         if not self.genAndInsertIV(byte_array):
             return False
         if not self.genAndInsertChecksum(byte_array):
@@ -102,7 +101,7 @@ class Crypt:
             self.enc_dec_byteArray(byte_array, round, True)
         return True
 
-    def decrypt(self, byte_array: list[byte]) -> bool:
+    def decrypt(self, byte_array: bytearray) -> bool:
         for round in range(ROUNDS - 1, -1, -1):
             self.enc_dec_byteArray(byte_array, round, False)
         if not self.de_ob_fuscateWithIV(byte_array):
@@ -114,7 +113,7 @@ class Crypt:
         byte_array[:] = byte_array[IV_SIZE:]
         return True
 
-    def setKeys(self, byte_array: list[byte]) -> None:
+    def setKeys(self, byte_array: bytearray) -> None:
         self.__keys[0] = byte_array[:KEY_SIZE]
         self.generateSubkeys()
     
--- Python/CryptLib/Test.py
+++ Python/CryptLib/Test.py
@@ -1,12 +1,9 @@
 from Crypt import Crypt
-from ctypes_ import UInt8 as byte
 
-arr = [byte(1),byte(2),byte(3),byte(4),byte(5),byte(6),byte(7),byte(8),byte(9),byte(10)]
+arr = bytearray([1, 2, 3, 4, 10, 5, 6, 7, 8, 9, ])
 cr = Crypt()
 
-key = [0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF, 0x01, 0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF, 0x01]
-for i in range(len(key)):
-    key[i] = byte(key[i])
+key = bytearray([0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF, 0x01, 0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF, 0x01])
 cr.setKeys(key)
 
 print("Before: ",arr)
@@ -15,11 +12,7 @@ print("Encrypted: ",arr)
 cr.decrypt(arr)
 print("Decrypted: ",arr)
 
-
-
-encr = [104, 141, 136, 254, 253, 128, 196, 9, 14, 27, 1, 153, 92, 230, 172, 178, 166, 28, 235, 173, 93, 155, 232, 121, 175, 5, 188]
-for i in range(len(encr)):
-    encr[i] = byte(encr[i])
+encr = bytearray([104, 141, 136, 254, 253, 128, 196, 9, 14, 27, 1, 153, 92, 230, 172, 178, 166, 28, 235, 173, 93, 155, 232, 121, 175, 5, 188])
 
 res = cr.decrypt(encr)
 print("Other Decrypted: ", encr if res else "Failed")
